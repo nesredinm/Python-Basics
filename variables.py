ff85c8b32505e0243cf7a5748db69a0a9f592277
Nesre <nesredin.m11@gmail.com>
2026-01-16 20:48:07 +0300

    variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+name = "Hammad"
+print(name)
+
+_age = 18 # valid
+
+# you can not do:
+# 9name = "Hammad"
+# did you know why?
+
+name_6_name = "Hammad"
+
+# any letter, number and under_score can be used to name a variable. but number can not used at the beginning.
+
+love = not False
+print (f"Love: {love}")
+
+status = not []
+print (f"status: {status}")
