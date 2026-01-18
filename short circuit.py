659337778c462f7c6dad725f9adbbad6f9469bba
Nesre <nesredin.m11@gmail.com>
2026-01-18 21:20:08 +0300

    short circuit last commit

--- short circuit.py
+++ short circuit.py
@@ -4,6 +4,20 @@ a = 0 and 1/0 # what is the output? - having fun with py!
 b = 1 or 1/0
 
 
-print(a,"\n",b)
+print(a,b)
 
+# an error:
+"""ab = 1/0 and 0
+print (ab)"""
 
+# what about this?
+c = 0 and 1
+d = 0 or 1
+
+print(c, d)
+
+# And this
+e = 1 and 2
+f = 1 or 2
+
+print(e, f)
